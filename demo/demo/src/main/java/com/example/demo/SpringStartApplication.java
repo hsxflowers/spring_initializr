fffd36aea05917c5658e6738b7fefc24e66f892a
Giovanna Bregantin <99514283+hsxflowers@users.noreply.github.com>
2023-08-28 22:49:42 +0000

    hello world

--- demo/demo/src/main/java/com/example/demo/SpringStartApplication.java
+++ demo/demo/src/main/java/com/example/demo/SpringStartApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpringStartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStartApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "msg"
 	}
 
 }
